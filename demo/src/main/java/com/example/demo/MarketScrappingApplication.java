d158fe0d5c6feee64ea9d4b7aeaa9173e97a90ef
Ömer Faruk <omerbyrktrts220@gmail.com>
2024-12-11 20:30:48 +0300

    last update

--- demo/src/main/java/com/example/demo/MarketScrappingApplication.java
+++ demo/src/main/java/com/example/demo/MarketScrappingApplication.java
@@ -1,14 +1,14 @@
-package com.example.demo;
+package com.example.demo;  //Bu proje için oluşturulan paket adı
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.beans.factory.annotation.Autowired; //Spring Framework'ün otomatik olarak bağlamaları yapabilmesi için kullanılan anotasyon
+import org.springframework.boot.CommandLineRunner; //Spring Boot uygulamalarının başlatılmasını sağlayan arayüz
+import org.springframework.boot.SpringApplication; //Spring Boot uygulamalarının başlatılmasını sağlayan sınıf
+import org.springframework.boot.autoconfigure.SpringBootApplication; //Spring Boot uygulamalarının otomatik yapılandırılmasını sağlayan anotasyon
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories; //MongoDB veritabanı işlemleri için kullanılan anotasyon
 
-import com.example.demo.repository.A101DataRepository;
-import com.example.demo.repository.MigrosDataRepository;
-import com.example.demo.repository.SokDataRepository;
+import com.example.demo.repository.A101DataRepository; //A101 verilerinin veritabanına kaydedilmesi için kullanılan repository sınıfı
+import com.example.demo.repository.MigrosDataRepository; //Migros verilerinin veritabanına kaydedilmesi için kullanılan repository sınıfı
+import com.example.demo.repository.SokDataRepository; //Şok verilerinin veritabanına kaydedilmesi için kullanılan repository sınıfı
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -32,16 +32,17 @@ public class MarketScrappingApplication implements CommandLineRunner {
         SokScraper sokScraper = new SokScraper(sokDataRepository);
         Thread sokThread = new Thread(sokScraper);
         sokThread.start();
-        sokThread.join();
 
         A101Scraper a101scraper = new A101Scraper(a101DataRepository);
         Thread a101Thread = new Thread(a101scraper);
         a101Thread.start();
-        a101Thread.join();
 
         MigrosScraper migrosScraper = new MigrosScraper(migrosDataRepository);
         Thread migrosThread = new Thread(migrosScraper);
         migrosThread.start();
+
+        sokThread.join();
+        a101Thread.join();
         migrosThread.join();
     }
 }
